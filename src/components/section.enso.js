2ef73f1a22f61a194790229d656bf2f7c65876cd
sean <seanyoung247@gmail.com>
2025-12-08 23:57:49 +0000

    feat: Added section component

---
+++ src/components/section.enso.js
@@ -0,0 +1,19 @@
+
+import Enso, { css, html } from 'ensojs';
+import Reset from "../styles/reset.css?inline";
+
+
+Enso.component('enso-section', {
+    styles: [css(Reset),
+    css`
+        section {
+            padding: 1em 2em;
+            border-bottom: 2px solid var(--stroke-color);
+        }
+    `],
+    template: html`
+        <section>
+            <slot></slot>
+        </section>
+    `
+});
