d258281a7a3eddd4a24f98d67d0eef9de2392046
sean <seanyoung247@gmail.com>
2025-12-08 23:57:02 +0000

    feat: Added nav button component

---
+++ src/components/navbtn.enso.js
@@ -0,0 +1,83 @@
+
+import Enso, { attr, css, html } from 'ensojs';
+import Reset from "../styles/reset.css?inline";
+
+
+Enso.component('nav-btn', {
+    watched: { open: attr(false) },
+    styles: [css(Reset),
+        css`
+            :host {
+                display: block;
+                position: fixed;
+                top: 0; right: 0;
+            }
+            button {
+                width: 40px;
+                height: 40px;
+                background: transparent;
+                border: none;
+            }
+            .line {
+                stroke: var(--primary-text);
+                stroke-linecap: round;
+                stroke-width: 6;
+            }
+            .enso {
+                fill: none;
+                stroke: var(--primary-text);
+                stroke-width: 8;
+                stroke-linecap: round;
+                stroke-dasharray: 200 220;
+                stroke-dashoffset: 200;
+                transition: stroke-dashoffset 0.5s ease;
+            }
+            .line.top, .line.bottom {
+                transform-origin: 50% 50%;
+                transition: transform 0.5s ease;
+            }
+            .line.middle {
+                stroke-dasharray: 65 65;
+                stroke-dashoffset: 0;
+                transition: stroke-dashoffset 0.5s ease 0.2s;
+            }
+
+            :host([open]) { outline: none; }
+            :host([open]) .enso {
+                stroke-dashoffset: 10;
+                transition: stroke-dashoffset 0.5s ease 0.2s;
+            }
+            :host([open]) .line.top, :host([open]) .line.bottom {
+                --tY: 12.5%;
+                --tR: 45deg;
+                transform:
+                    translateY(var(--tY))
+                    translateX(-10%)
+                    rotate(var(--tR))
+                    scale(0.8);
+                transition: transform 0.5s ease 0.1s;
+            }
+            :host([open]) .line.bottom {
+                --tY: -12.5%;
+                --tR: -45deg;
+            }
+            :host([open]) .line.middle {
+                stroke-dashoffset: -65;
+                transition: stroke-dashoffset 0.5s ease;
+            }
+
+        `
+    ],
+    template: html`
+        <button @click="() => @:open = !@:open" aria-label="Toggle Navigation Menu">
+            <svg viewBox="0 0 100 100">
+                <line class="line top"    x1="20" y1="30" x2="80" y2="30" />
+                <line class="line middle" x1="20" y1="50" x2="80" y2="50" />
+                <line class="line bottom" x1="20" y1="70" x2="80" y2="70" />
+
+                <circle class="enso" cx="50" cy="50" r="35" />
+            </svg>
+        </button>
+    `
+});
+
