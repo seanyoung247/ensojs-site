23e0b473dea1a7eb44ec255ba6b526e968cecd98
sean <seanyoung247@gmail.com>
2025-12-08 23:59:01 +0000

    feat: Added app component

---
+++ src/app.enso.js
@@ -0,0 +1,71 @@
+
+import Enso, { css, html } from 'ensojs';
+// Components
+import "./components/nav.enso";
+import "./components/icon.enso";
+import "./components/header.enso";
+import "./components/section.enso";
+import "./components/codepane.enso";
+import "./components/counter.enso";
+
+// Styles
+import Styles from "./app.css?inline";
+import "./styles/main.css";
+
+const code = `
+Enso.component("tiny-counter", &#123
+    watched: &#123 value: attr(0) &#125;,
+    styles: css\`:host&#123
+        display:flex;
+        justify-content:space-between;
+    &#125;\`,
+    template: html\`
+        &lt;button @click="()=>@:value--"&gt;-&lt;/button&gt;
+        &#123;&#123; @:value &#125;&#125;
+        &lt;button @click="()=>@:value++"&gt;+&lt;/button&gt;
+    \`
+&#125;);`;
+
+
+console.log(Enso.version);
+
+Enso.component("enso-app", {
+    styles: css(Styles),
+    template: html`
+        <enso-header class="section">
+            <enso-nav></enso-nav>
+            <enso-icon></enso-icon>
+            <h1>Enso</h1>
+        </enso-header>
+        <enso-section>
+            <h2 id="tag-line">
+                <span>Minimal</span>
+                <span>Fast</span>
+                <span>Reactive</span>
+            </h2>
+            <p class="body-text">
+                A micro framework for writing declarative
+                components with modern simplicity.
+            </p>
+            <code-pane>
+                <code slot="code">
+                    ${  code  }
+                </code>
+                <div id="component-example" slot="live">
+                    <tiny-counter></tiny-counter>
+                </div>
+            </code-pane>
+        </enso-section>
+        <enso-section>
+            <h3>Why Enso?</h3>
+            <ul>
+                <li>Built to disappear</li>
+                <li>Write components the way they should feel</li>
+                <li>No build required</li>
+                <li>Small, 7kb gzipped</li>
+                <li>Reactive data binding</li>
+                <li>Truly encapsulated styles and markup</li>
+            </ul>
+        </enso-section>
+    `
+});
