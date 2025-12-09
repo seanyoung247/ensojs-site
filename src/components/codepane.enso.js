bab4eedec9380ffcfd51c03222534d9530c87f6b
sean <seanyoung247@gmail.com>
2025-12-09 14:42:13 +0000

    feat: Added code highlighting using Prism

--- src/components/codepane.enso.js
+++ src/components/codepane.enso.js
@@ -1,21 +1,41 @@
 
 import Enso, { css, html } from 'ensojs';
+import Prism from 'prismjs';
+
 import Reset from "../styles/reset.css?inline";
 
 
 Enso.component('code-pane', {
     styles: [css(Reset),
     css`
-        slot[name="code"] {
+        :host {
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            overflow: hidden;
+        }
+        slot {
             display: block;
             width: 100%;
-            box-sizing: border-box;
-            padding: 0.5em;
-            background: var(--code-back);
         }
     `],
     template: html`
-        <slot name="code"></slot>
+        <slot name="code" @slotchange="this.slotChange"></slot>
         <slot name="live"></slot>
-    `
+    `,
+    script: {
+        slotChange(event) {
+            const slot = event.target;
+            const nodes = slot.assignedNodes();
+            for (const node of nodes) {
+                if (node.nodeType === Node.ELEMENT_NODE) {
+                    const lang = (node.getAttribute('data-lang') || 'javascript')
+                        .split(',').map(l => `language-${l}`);
+
+                    node.classList.add(...lang);
+                    Prism.highlightElement(node);
+                }
+            }
+        }
+    }
 });
