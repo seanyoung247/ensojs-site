0361ed1aba323bfab9a715040484be699a0c892b
sean <seanyoung247@gmail.com>
2025-12-18 20:50:51 +0000

    refactor: Moved nav code to component

--- src/app.enso.js
+++ src/app.enso.js
@@ -1,20 +1,17 @@
 
-import Enso, { css, html, watches, lifecycle } from 'ensojs';
+import Enso, { css, html } from 'ensojs';
 // Components
-import "./components/nav.enso";
 import "./components/icon.enso";
 import "./components/header.enso";
-import "./components/themeSwitch.enso";
+
 // Sections
+import Nav from "./sections/nav.enso"
 import Example from "./sections/example.enso"
 import WhyEnso from "./sections/why.enso"
 
 // Styles
 import Styles from "./app.css?inline";
 import "./styles/main.css";
-
-// Images
-import ThemeIcons from "/theme_icons.svg";
 
 
 console.log(Enso.version);
@@ -24,36 +21,7 @@ Enso.component("enso-app", {
     settings: { useShadow: false },
     styles: css(Styles),
     template: html`
-        <site-nav #ref="nav">
-            <li class="nav-section">
-                <h2>On this page</h2>
-                <ul>
-                    <li class="nav-item" @click="this.closeNav">
-                        <a href="#why-enso">Why Enso?</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#overview">Overview</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="nav-section">
-                <h2>Explore</h2>
-                <ul>
-                    <li class="nav-item">Dummy Link</li>
-                    <li class="nav-item">Dummy Link</li>
-                    <li class="nav-item">Dummy Link</li>
-                </ul>
-            </li>
-            <li class="nav-section">
-                <h2>Settings</h2>
-                <ul>
-                    <li class="nav-item theme">
-                        <span>Theme: </span>
-                        <theme-switch #ref="themer"></theme-switch>
-                    </li>
-                </ul>
-            </li>
-        </site-nav>
+        ${ Nav }
         <site-header>
             <enso-icon></enso-icon>
             <h1>Enso</h1>
@@ -61,16 +29,4 @@ Enso.component("enso-app", {
         ${ Example }
         ${ WhyEnso }
     `,
-    script: { 
-        setup: watches(function() {
-            this.refs.themer.watched.themes = [
-                {name: 'light', icon:`${ThemeIcons}#light`}, 
-                {name: 'dark', icon:`${ThemeIcons}#dark`},
-                {name: 'auto', icon:`${ThemeIcons}#auto`}
-            ];
-        }, [lifecycle.mount]),
-        closeNav() {
-            this.refs.nav.close();
-        }
-    }
 });
