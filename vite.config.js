d89740f9af00a43116f4c0396464f11c21e95fd3
Sean Young <seanyoung247@gmail.com>
2025-12-28 23:49:56 +0000

    chore: Added path aliases

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+    }
+  }
+});
