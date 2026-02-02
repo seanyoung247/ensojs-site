c57a9557ab06aa730f277749a90806d150d29f1c
sean <seanyoung247@gmail.com>
2026-02-02 18:53:04 +0000

    refactor: Updated code to enso RC17

--- src/components/nav/themes.enso.js
+++ src/components/nav/themes.enso.js
@@ -1,21 +1,17 @@
 
 
-import Enso, { html, watches, lifecycle } from 'ensojs';
+import Enso, { html } from 'ensojs';
 import "@components/themeSwitch.enso";
 // Images
 import ThemeIcons from "/theme_icons.svg";
 
 export default Enso.component('enso-theme-switch', {
     template: html`
-        <theme-switch #ref="themer"></theme-switch>
+        <theme-switch .themes="[
+                {name: 'light', icon:'${ThemeIcons}#light'}, 
+                {name: 'dark', icon:'${ThemeIcons}#dark'},
+                {name: 'auto', icon:'${ThemeIcons}#auto'}
+            ]" 
+        ></theme-switch>
     `,
-    script: {
-        setup: watches(function() {
-            this.refs.themer.watched.themes = [
-                {name: 'light', icon:`${ThemeIcons}#light`}, 
-                {name: 'dark', icon:`${ThemeIcons}#dark`},
-                {name: 'auto', icon:`${ThemeIcons}#auto`}
-            ];
-        }, [lifecycle.mount]),
-    }
 });
