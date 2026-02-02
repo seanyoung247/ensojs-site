5d8673660d106149b9da0893a998866d1134d870
sean <seanyoung247@gmail.com>
2026-02-02 18:36:29 +0000

    feat: Initial docs

--- src/docs/spa.enso.js
+++ src/docs/spa.enso.js
@@ -2,13 +2,6 @@
 import Enso, { css, html } from 'ensojs';
 
 
-function getSlug() {
-    const path = location.pathname.replace(/\/$/, '');
-    const slug = path.split('/').pop();
-    return slug || 'intro';
-}
-
-
 Enso.component("enso-spa", {
     settings: { useShadow: false },
     styles: css`
