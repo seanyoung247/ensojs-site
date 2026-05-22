69e41fe6a56d991f250cd007abed1cb21fb03bdc
Sean Young <seanyoung247@gmail.com>
2026-05-22 12:45:07 +0100

    Updates

---
+++ assets/testpage2.enso-KUgENA5R.js
@@ -0,0 +1,26 @@
+import{L as e,b as a,i as t,R as s}from"./reset-CKXpDUjB.js";const o=e.component("test-page-2",{settings:{useShadow:!1},styles:[t(s),t`
+        test-page-2 {
+            display: block;
+            width: 100%;
+            height: fit-content;
+            padding: 2rem 1rem;
+            box-sizing: border-box;
+            color: var(--primary-text);
+        }
+
+        h1 {
+            font-size: 2.5rem;
+            text-align: center;
+            margin-bottom: 1rem;
+        }
+
+        p {
+            font-size: 1.125rem;
+            text-align: center;
+            max-width: 600px;
+            margin: 0 auto;
+        }
+    `],template:a`
+        <h1>Test Page 2</h1>
+        <p>This is a test page!</p>
+    `});export{o as default};
