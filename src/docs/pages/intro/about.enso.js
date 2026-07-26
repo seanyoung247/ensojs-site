fae682135ba9a3da26c44ebac4b37c7136fe8c1d
Your Name <you@example.com>
2026-07-26 21:55:32 +0100

    feat: Added history section to the the About page

--- src/docs/pages/intro/about.enso.js
+++ src/docs/pages/intro/about.enso.js
@@ -1,11 +1,134 @@
 
 import Enso, { css, html } from 'ensojs';
+
 import Reset from "@styles/reset.css?inline";
+
+
+export const webComponentCode = `
+    <span class="code-line">
+        <span class="token keyword">class</span>
+        <span class="token property"> MyComponent </span>
+        <span class="token keyword">extends</span>
+        <span class="token property"> WebComponent </span>
+        <span class="token punctuation">{</span>
+    </span>
+
+    <span class="code-line">
+        <span class="space">    </span>
+        <span class="token keyword">static</span>
+        <span class="token function"> get</span>
+        <span class="token property"> tagName</span>
+        <span class="token punctuation">() {</span>
+        <span class="token keyword"> return</span>
+        <span class="token string"> 'my-component'</span>
+        <span class="token punctuation">; }</span>
+    </span>
+
+    <span class="code-line">
+        <span class="space">    </span>
+        <span class="token keyword">static</span>
+        <span class="token function"> get</span>
+        <span class="token property"> attributes</span>
+        <span class="token punctuation">() {</span>
+    </span>
+
+    <span class="code-line">
+        <span class="space">        </span>
+        <span class="token keyword">return</span>
+        <span class="token punctuation"> {</span>
+    </span>
+
+    <span class="code-line">
+        <span class="space">            </span>
+        <span class="token string">'value'</span>
+        <span class="token punctuation">: {</span>
+        <span class="token property">type</span>
+        <span class="token punctuation">: </span>
+        <span class="token property">Number</span>
+        <span class="token punctuation">, </span>
+        <span class="token property">default</span>
+        <span class="token punctuation">: </span>
+        <span class="token number">0</span>
+        <span class="token punctuation">}</span>
+    </span>
+
+    <span class="code-line">
+        <span class="space">        </span>
+        <span class="token punctuation">};</span>
+    </span>
+
+    <span class="code-line">
+        <span class="space">    </span>
+        <span class="token punctuation">}</span>
+    </span>
+
+    <span class="code-line">
+        <span class="space">    </span>
+        <span class="token comment">// component logic</span>
+    </span>
+
+    <span class="code-line">
+        <span class="token punctuation">}</span>
+    </span>
+`;
+
+export const ensoClassCode = `
+    <span class="code-line">
+        <span class="token keyword">class</span>
+        <span class="token property"> MyComponent </span>
+        <span class="token keyword">extends</span>
+        <span class="token property"> Enso </span>
+        <span class="token punctuation">{</span>
+    </span>
+
+    <span class="code-line">
+        <span class="space">    </span>
+        <span class="token keyword">static</span>
+        <span class="token function"> get</span>
+        <span class="token property"> tagName</span>
+        <span class="token punctuation">() {</span>
+        <span class="token keyword"> return</span>
+        <span class="token string"> 'my-component'</span>
+        <span class="token punctuation">; }</span>
+    </span>
+
+    <span class="code-line">
+        <span class="space">    </span>
+        <span class="token comment">// component logic</span>
+    </span>
+
+    <span class="code-line">
+        <span class="token punctuation">}</span>
+    </span>
+`;
+
+export const ensoComponentCode = `
+    <span class="code-line">
+        <span class="token property">Enso</span>
+        <span class="token punctuation">.</span>
+        <span class="token function">component</span>
+        <span class="token punctuation">(</span>
+        <span class="token string">'my-component'</span>
+        <span class="token punctuation">, {</span>
+    </span>
+
+    <span class="code-line">
+        <span class="space">    </span>
+        <span class="token comment">// Component declaration</span>
+    </span>
+
+    <span class="code-line">
+        <span class="token punctuation">});</span>
+    </span>
+`;
 
 
 export default Enso.component('intro-about-page', {
     settings: { useShadow: false },
     styles: [css(Reset), css`
+        h1, h2 {
+            padding: 0.5em 0;
+        }
         ul.philosophy {
             padding-left: 1.5em;
         }
@@ -16,6 +139,9 @@ export default Enso.component('intro-about-page', {
                 width: 1.5em;
                 text-align: center;
             }
+        }
+        code.code-pane {
+            padding: 0.1em;
         }
     `],
     template: html`
@@ -44,13 +170,36 @@ export default Enso.component('intro-about-page', {
             <li><span>📦</span>ESM-first, no legacy module formats
             <li><span>🔌</span>Extensible template pipeline — define custom attribute handlers or parsing steps
         </ul>
+
+        <h2 id="history">History</h2>
+        <p>Enso began life as a small utility class intended to reduce the repetitive boilerplate associated with writing Web Components. You simply extended from it, and added your own code.</p>
+        <code class="code-pane">
+            ${webComponentCode}
+        </code>
+        <p>
+            As real-world components grew, new sources of repetitive boilerplate appeared. For instance, querying the DOM for child elements. To solve this, a quick and simple processing
+            step was added to the template parsing, to extract #ref="myRef" attributes, and insert them as fields on the component, for simple access via the class: this.myRef. But it soon
+            became apparent that there was still a great deal of boilerplate, attaching events and the @<event>="" attribute evolved to replace calls to this.myRef.addEventListener(...). 
+            As more features were added, Enso started to form.
+        </p>
+        <br/>
+        <p>From:</p>
+        <code class="code-pane">
+            ${ensoClassCode}
+        </code>
+        <p>To the current declarative:</p>
+        <code class="code-pane">
+            ${ensoComponentCode}
+        </code>
+        <p>Enso today continues that original spirit: remove friction, embrace clarity, let components express themselves naturally.</p>
     `,
     script: {
         getHeadings() {
             return [
                 { title: "About", link: "#about" },
                 { title: "Philosophy", link: "#philosophy" },
-                { title: "Features", link: "#features" }
+                { title: "Features", link: "#features" },
+                { title: "History", link: "#history" }
             ]
         }
     }
