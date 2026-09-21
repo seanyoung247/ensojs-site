a5b350eb5fa76b5fb9fe5b4ee0f4509d1e7eb3fd
Your Name <you@example.com>
2026-09-21 23:08:31 +0100

    feat: Added initial template page layout

--- src/docs/pages/components/templates.enso.js
+++ src/docs/pages/components/templates.enso.js
@@ -10,11 +10,40 @@ import DocStyles from "@styles/documentation.css?inline";
 export default Enso.component('components-templates-page', {
     settings: { useShadow: false },
     styles: [css(Reset), css(DocStyles)],
-    template: html``,  
+
+    template: html`
+        <div class="document">
+            <section id="#component-templates-field">
+                <h1>Templates</h1>
+                <!-- template: -->
+                <!-- html -->
+                <!-- {{}} -->
+                <!-- enso:ignore, enso:ignore-children -->
+
+            </section>
+            <section id="#component-templates-bindings">
+                <h2>Bindings</h2>
+                <!-- attributes :attribute, enso-attr:attribute -->
+                <!-- properties .property, enso-prop:property -->
+                <!-- references #<ref name>, enso-event:<ref name> -->
+                <!-- events @<event name>, enso-event:<event name> -->
+
+            </section>
+            <section id="#component-templates-directives">
+                <h2>Directives</h2>
+                <!-- *if, enso-if -->
+                <!-- *for, enso-for -->
+
+            </section>
+        </div>
+    `,  
+
     script: {
         getHeadings() {
             return [
-                { title: "", link: "#" },
+                { title: "Templates", link: "#component-templates-field" },
+                { title: "Bindings", link: "#component-templates-bindings" },
+                { title: "Directives", link: "#component-templates-directives" }
             ];
         },
         getSection() {
